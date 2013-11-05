0d325b7bcf7eae567ea353cd15e426617b1b6cd2
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-11-05 13:31:10 -0600

    Adding a knockout demo project

---
+++ KnockoutDemo/KnockoutDemo/Scripts/app/person.js
@@ -0,0 +1,26 @@
+﻿require([], function () {
+
+   $(function () {
+
+      var vm = function () {
+         var first = ko.observable(''),
+             last = ko.observable(''),
+             fullName = ko.computed(function () {
+                return first() + " " + last();
+             }, this),
+             hideWelcome = ko.computed(function () {
+                return !first() && !last();
+             }, this);
+
+         return {
+            first: first,
+            last: last,
+            fullName: fullName,
+            hideWelcome: hideWelcome
+         };
+      };
+
+      ko.applyBindings(vm());
+   });
+
+});
