53462cb2190d085426fb58b2b37c3f6fedf337ef
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-11-06 10:00:36 -0600

    Adding an observable example

---
+++ KnockoutDemo/KnockoutDemo/Scripts/app/people.js
@@ -0,0 +1,38 @@
+﻿require([], function() {
+
+   var person = function(name) {
+      this.name = name;
+   };
+
+   var vm = function() {
+
+      var name = ko.observable();
+      
+      var add = function() {
+         people.push(new person(name()));
+      };
+
+      var people = ko.observableArray([
+            new person('justin'),
+            new person('kenny'),
+            new person('nik')
+      ]);
+      
+      var selectedPerson = ko.observable(people()[0]);
+
+      var select = function(person) {
+         selectedPerson(person);
+      };
+
+      return {
+         name: name,
+         add:add,
+         people: people,
+         selectedPerson:selectedPerson,
+         select:select
+      };
+   };
+
+   ko.applyBindings(vm());
+
+});
